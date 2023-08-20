31f74be4f28b86bd861ddda01abfc918ad5077c1
Emmanuel Bonilla <bonillaem10@gmail.com>
2023-08-20 00:22:16 -0300

    feact: Adding web server, with calculator get request, you can be able to subsctract, multiply, division, and addicion

---
+++ calc/operations.py
@@ -0,0 +1,12 @@
+def add(first_number, second_number):
+    return first_number + second_number
+
+def mult(first_number, second_number):
+    return first_number * second_number
+
+def div(first_number, second_number):
+    return first_number / second_number
+
+def sub(first_number, second_number):
+    return first_number - second_number
+
